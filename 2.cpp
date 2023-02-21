3b35a28863a8fe416c6998b9350bdf139dc2dbb7
verma-divyanshu-git <96394150+verma-divyanshu-git@users.noreply.github.com>
2023-02-22 02:54:02 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,3 +1,21 @@
+/* Given a string 'S'. Write a program to find an array of all possible strings in any order that can be formed by transforming every letter individually to the lowercase or the uppercase.
+Note:
+1. You can print the array in any order.
+2. The string 'S' only contains English alphabets and digits.
+
+Sample Input 1:
+a1b
+Sample Output 1:
+A1B A1b a1B a1b
+
+Sample Input 2:
+0
+Sample Output 2:
+0
+Explanation: There is no alphabet in the string, so we get the output same as the input string.
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
